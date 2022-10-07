13db799cced4e2c8eeec2f996933ed4502d76608
Yana Kuzina <kuzina25.10.2013@gmail.com>
2022-10-07 07:45:23 +0300

    Create component Title

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <div>
+      <Title name="Yana" type="bold" />
+      <Title name="Iriska" type="normal" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+const Title = ({ name, type }) => {
+  return <p style={{ fontWeight: type }}>{name}</p>;
+};
+
+export default Title;
